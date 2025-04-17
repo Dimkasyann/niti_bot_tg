e429cf465ebdb233d0e590ec3930accfd40bebbc
Dimkasyann <laptev.dimka.91@gmail.com>
2025-04-17 16:53:16 +0300

    __init__.py

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from aiogram import Router
+from .admin import admin_router
+from .puzzles import puzzles_router
+from .rating import rating_router
+
+router = Router()
+router.include_router(admin_router)
+router.include_router(puzzles_router)
+router.include_router(rating_router)
